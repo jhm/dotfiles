ffa49d4ec78ac72793f7e2ebfc4a849ddad354e8
John Major <code@johnmajor.ca>
2016-03-27 02:12:56 -0400

    Moved emacs configuration to org files.
    This is the inital move of my emacs configuration files to org files.
    Some of the org files are still incomplete and have yet to be
    documented.

--- all/dots/.emacs.d/init.el
+++ all/dots/.emacs.d/init.el
@@ -1,100 +1,11 @@
-(require 'cl)
-(require 'package)
-
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
 ;; Show traces for all tramp messages.
 (setq tramp-verbose 10)
 
-(defvar my-packages
-  '(powerline
-    ;; Themes
-    solarized-theme
-    twilight-theme
-    zenburn-theme
-    ;; VCS
-    magit
-    ;; Elixir
-    elixir-mode
-    elixir-yasnippets
-    alchemist
-    ;; Clojure
-    clojure-mode
-    cider
-    ac-nrepl
-    ;; Markup
-    markdown-mode
-    yaml-mode
-    ;; Haskell
-    flymake-hlint
-    haskell-mode
-    ghci-completion
-    ;; Lisp
-    elisp-slime-nav
-    paredit
-    parenface
-    ;; Scala
-    scala-mode2
-    ;; Javascript
-    flymake-jslint
-    js-comint
-    js2-mode
-    ac-js2
-    ;; Python
-    flymake-python-pyflakes
-    nose
-    pep8
-    pylint
-    pymacs
-    python
-    ;; Misc
-    ac-dabbrev
-    ack
-    idle-highlight-mode
-    ido-ubiquitous
-    neotree
-    popup
-    rainbow-mode
-    smex
-    sr-speedbar
-    yasnippet)
-  "Packages to ensure are installed at launch.")
-
-;; Directory that stores my customization files.
 (setq config-dir (file-truename (concat user-emacs-directory "config/")))
+;;(add-to-list 'load-path config-dir)
 
 ;; Where machine specific customization information is stored
 ;; (custom-set-variables, custom-set-faces etc.).
 (setq custom-file (concat config-dir "custom.el"))
 
-(defvar configs
-  `("global"
-    "functions"
-    "haskell"
-    "js"
-    "lisp"
-    "python")
-  "Customization files that will be loaded at launch.")
-
-;; Install any missing packages.
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
-;; Load my customizations.
-(dolist (config configs)
-  (load (concat config-dir config ".el")))
-
-;; Fix path issues.
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-(when window-system (set-exec-path-from-shell-PATH))
+(org-babel-load-file (concat config-dir "core.org"))
